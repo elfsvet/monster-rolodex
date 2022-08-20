8c07ca0266848f8ff99bd4a21dafe393c6cb6935
elfsvet <stepanmatysik@gmail.com>
2022-08-19 23:28:37 -0400

    stopped at 43rd

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
 import logo from './logo.svg';
 import './App.css';
-// component ui representation of 
-function App() {
+import { Component } from 'react';
+// component ui representation of
+class App extends Component {
   // this is function that returning the html code
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hey I'm stepan
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // constructor call - runs first
+  constructor() {
+    super();
+
+    this.state = {
+      // initialize a state
+      monsters: [],
+      sarchedField: '',
+    };
+    console.log('constructor initial state');
+  }
+  // life cycle methods runs third
+  // did mount it runs
+
+  componentDidMount() {
+    console.log('component Did Mount');
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) =>
+        // after setState called render rerender the page
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
+  // render - runs second what to show initial ui mount on the DOM
+  filterMonsters(element) {
+    return console.log(this.state.monsters.includes('l'));
+  }
+
+  render() {
+    console.log('render');
+
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      //  if the name includes the search string return the monster to the array
+      return monster.name.toLowerCase().includes(this.state.searchField);
+    });
+
+    return (
+      <div className='App'>
+        <input
+          className='search-box'
+          type='search'
+          placeholder='search monsters'
+          onChange={(e) => {
+            // filter true and false filter
+            const searchField = e.target.value.toLowerCase();
+            this.setState(
+              () => {
+                return { searchField };
+              },
+              () => {}
+            );
+          }}
+        />
+        {filteredMonsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
